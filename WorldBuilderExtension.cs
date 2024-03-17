fda2c51ac4ad14a9f9c7bd9f8ee6872c2f1b625c
Gearlink0 <gearlinkgmaster@aol.com>
2024-03-17 13:53:06 -0600

    Updated WorldBuilderExtension to run FortunadoSpawnerBuilder directly for sake of simplicity

--- WorldBuilderExtension.cs
+++ WorldBuilderExtension.cs
@@ -1,5 +1,6 @@
 using Genkit;
 using XRL.World;
+using XRL.World.Parts;
 
 namespace XRL.World.WorldBuilders
 {
@@ -21,13 +22,8 @@ namespace XRL.World.WorldBuilders
 			Location2D location = builder.popMutableLocationOfTerrain("Ruins");
       // Get that zone's ID.
 			string zoneID = Zone.XYToID(this.World, location.X, location.Y, 10);
-      /* Get a random empty cell that is reachable where we will add an object.
-      Due to the nature of the object we're adding, it probably isn't striclty
-      necessary to ensure the cell is empty and reachable, but this would be important
-      if we were adding an objec the player was meant to see and interact with. */
-			Cell emptyCell = The.ZoneManager.GetZone(zoneID).GetEmptyReachableCells().RemoveRandomElement<Cell>();
-      // Actually add the object.
-			emptyCell.AddObject("Gearlink_CASKOFROSEWINE_FortunadoSpawner");
+      // Call the FortunadoSpawnerBuilder to build the encounter in the chosen zone.
+      new Gearlink_CASKOFROSEWINE_FortunadoSpawnerBuilder().BuildZone( The.ZoneManager.GetZone(zoneID) );
 
       /* Will log the ID of the zone being modified to the Player.log for debugging
       purposes. In game, you can use the wish goto:zoneID to teleport to the zone
